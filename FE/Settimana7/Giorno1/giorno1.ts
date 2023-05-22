b67c5c6e7fa3f6e73d22769a41a4d0ce8a754148
Davide Spinozzi <davidespinozzi01@gmail.com>
2023-05-22 16:00:54 +0200

    .

--- FE/Settimana7/Giorno1/giorno1.ts
+++ FE/Settimana7/Giorno1/giorno1.ts
@@ -1,26 +1,25 @@
 function Confronto(numero1: number, numero2: number): string {
-  const numeroCasuale = Math.floor(Math.random() * 100) + 1;
-  console.log(numeroCasuale);
-  const differenza1 = Math.abs(numero1 - numeroCasuale);
-  const differenza2 = Math.abs(numero2 - numeroCasuale);
-
-  if (numero1 === numeroCasuale) {
-    return "Giocatore 1 ha azzeccato";
-  } else if (numero2 === numeroCasuale) {
-    return "Giocatore 2 ha azzeccato";
-  } else {
-    if (differenza1 < differenza2) {
-      return "Giocatore 1 è più vicino";
-    } else if (differenza2 < differenza1) {
-      return "Giocatore 2 è più vicino";
+    let numeroCasuale = Math.floor(Math.random() * 100) + 1;
+    console.log(numeroCasuale);
+    let differenza1 = Math.abs(numero1 - numeroCasuale);
+    let differenza2 = Math.abs(numero2 - numeroCasuale);
+  
+    if (numero1 === numeroCasuale) {
+      return "Giocatore 1 ha azzeccato";
+    } else if (numero2 === numeroCasuale) {
+      return "Giocatore 2 ha azzeccato";
     } else {
-      return "Pareggio";
+      if (differenza1 < differenza2) {
+        return "Giocatore 1 è più vicino";
+      } else if (differenza2 < differenza1) {
+        return "Giocatore 2 è più vicino";
+      } else {
+        return "Pareggio";
+      }
     }
   }
-}
-
-const Giocatore1: number = 5;
-const Giocatore2: number = 64;
-const risultato: string = Confronto(Giocatore1, Giocatore2);
-console.log(risultato);
-    
+  
+  let Giocatore1 : number = 5;
+  let Giocatore2 : number= 64;
+  let risultato : string = Confronto(Giocatore1, Giocatore2);
+  console.log(risultato);
