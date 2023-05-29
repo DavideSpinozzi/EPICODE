c3777abdced87e0e7780092da95235c76aacdc6c
Davide Spinozzi <davidespinozzi01@gmail.com>
2023-05-29 14:42:25 +0200

    .

--- FE/Settimana8/Giorno1/giorno1/src/app/app.component.spec.ts
+++ FE/Settimana8/Giorno1/giorno1/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'primaApp'`, () => {
+  it(`should have as title 'giorno1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('primaApp');
+    expect(app.title).toEqual('giorno1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('primaApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('giorno1 app is running!');
   });
 });
--- FE/Settimana8/Giorno1/giorno1/src/app/app.component.ts
+++ FE/Settimana8/Giorno1/giorno1/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'primaApp';
+  title = 'giorno1';
 }
