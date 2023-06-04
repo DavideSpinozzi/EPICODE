bbd1222e59b94fbb43d8fd1d3247478e12b35f5e
Davide Spinozzi <davidespinozzi01@gmail.com>
2023-06-04 12:29:36 +0200

    aggiunta modifica con prompt

--- FE/Settimana8/ProgettoAngular1/ProgettoAngular1/src/app/todo/todo.component.ts
+++ FE/Settimana8/ProgettoAngular1/ProgettoAngular1/src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../todo.service';
 import { Todo } from '../todo';
 
@@ -6,51 +6,63 @@ import { Todo } from '../todo';
   selector: 'app-todo',
   template: `
     <div class="container mt-3">
-  <div class="row justify-content-center">
-    <div class="col-4">
-    <ng-container *ngIf="showMessage">
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <ng-container *ngIf="showMessage">
             <p>Recupero Task...</p>
           </ng-container>
-      <table class="table table-striped" *ngIf="!showMessage">
-        <tbody>
-        <ng-container *ngIf="TaskIncomplete(); else noTasks">
-          <tr *ngFor="let todo of todos">
-            <ng-container *ngIf="!todo.completed;">
-
-              <th scope="row">{{ todo.id }}</th>
-              <td>{{ todo.title }}</td>
-              <td>
-                <div class="d-flex justify-content-end">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      fill="currentColor"
-                      class="bi bi-check-square-fill text-success ms-3"
-                      viewBox="0 0 16 16"
-                      (click)="completeTask(todo)"
-                    >
-                      <path
-                        d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      fill="currentColor"
-                      class="bi bi-x-square-fill text-danger ms-3"
-                      viewBox="0 0 16 16"
-                      (click)="deleteTask(todo)"
-                    >
-                      <path
-                        d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
-                      />
-                    </svg>
-                  </div>
-                </td>
-                </ng-container>
-              </tr>
+          <table class="table table-striped" *ngIf="!showMessage">
+            <tbody>
+              <ng-container *ngIf="TaskIncomplete(); else noTasks">
+                <tr *ngFor="let todo of todos">
+                  <ng-container *ngIf="!todo.completed">
+                    <th scope="row">{{ todo.id }}</th>
+                    <td>{{ todo.title }}</td>
+                    <td>
+                      <div class="d-flex justify-content-end">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          class="bi bi-pencil-fill text-primary"
+                          viewBox="0 0 16 16"
+                          (click)="ModificaTitolo(todo)"
+                        >
+                          <path
+                            d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
+                          />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          class="bi bi-check-square-fill text-success ms-3"
+                          viewBox="0 0 16 16"
+                          (click)="completeTask(todo)"
+                        >
+                          <path
+                            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"
+                          />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          class="bi bi-x-square-fill text-danger ms-3"
+                          viewBox="0 0 16 16"
+                          (click)="deleteTask(todo)"
+                        >
+                          <path
+                            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+                          />
+                        </svg>
+                      </div>
+                    </td>
+                  </ng-container>
+                </tr>
               </ng-container>
             </tbody>
           </table>
@@ -75,18 +87,16 @@ import { Todo } from '../todo';
         </div>
       </div>
       <ng-template #noTasks>
-    <p>Non ci sono task.</p>
-  </ng-template>
+        <p>Non ci sono task.</p>
+      </ng-template>
     </div>
   `,
   styles: [],
 })
-export class TodoComponent implements OnInit{
-
+export class TodoComponent implements OnInit {
   todos: Todo[] = [];
   nuovaTask: string = '';
   showMessage: boolean = true;
-
 
   constructor(private todoService: TodoService) {}
 
@@ -98,7 +108,7 @@ export class TodoComponent implements OnInit{
   }
 
   TaskIncomplete(): boolean {
-    return this.todos.some(todo => !todo.completed);
+    return this.todos.some((todo) => !todo.completed);
   }
 
   loadTasks(): void {
@@ -118,7 +128,7 @@ export class TodoComponent implements OnInit{
       this.nuovaTask = '';
     }
 
-    console.log(this.todos)
+    console.log(this.todos);
   }
 
   deleteTask(task: Todo): void {
@@ -128,5 +138,10 @@ export class TodoComponent implements OnInit{
   completeTask(task: Todo): void {
     task.completed = !task.completed;
   }
-
+  ModificaTitolo(task: Todo): void {
+    const newTitle = prompt('Modifica il titolo:', task.title);
+    if (newTitle !== null && newTitle.trim() !== '') {
+      task.title = newTitle.trim();
+    }
+  }
 }
